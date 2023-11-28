286d9213755344d98d4a9602dc807757a5df9f25
fasolobolo <146856162+fasolobolo@users.noreply.github.com>
2023-11-28 08:54:23 +0100

    Add files via upload

--- mp3-player.py
+++ mp3-player.py
@@ -2,8 +2,7 @@ import customtkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 from playsound import zagraj
-from pause import pauza
-
+import pygame
 app = tk.CTk()
 app.geometry("850x450")
 app.grid_columnconfigure(0, weight=1)
@@ -20,8 +19,15 @@ trv.heading("utwor", text="Tytuł")
 
 buton = tk.CTkButton(app, text="►", command=zagraj, width=10)
 buton.grid(row=0, column=0)
-buton2 = tk.CTkButton(app, text="pause", command=pauza)
-buton2.grid(row=1, column=0)
+a = 0
 
+def slider_event(value):
+    global a
+    a=(value)
+    
+slider = tk.CTkSlider(master=app, from_=0, to=100, command=slider_event)
+slider.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+pygame.mixer.music.set_volume(a)
 
 app.mainloop()

e88e8c097dd79283f306db07314417538e03b0e9
mati <MeenQ12@gmail.com>
2023-12-05 09:13:05 +0100

    Add files via upload

--- mp3player.py
+++ mp3player.py
@@ -9,6 +9,8 @@ app.geometry("850x450")
 app.grid_columnconfigure(0, weight=1)
 app.grid_rowconfigure(0, weight=1)
 
+
+nazwa_pliku_var = tk.StringVar()
 columns = "utwor"
 
 pygame.mixer.init()
@@ -17,27 +19,49 @@ pygame.display.init()
 
 dir_path = filedialog.askdirectory(title="Wybierz folder")
 playlista = []
+
 os.chdir(dir_path)
 dir_list = os.listdir(dir_path)
 files = 0
 while files < len(os.listdir(dir_path)):
-    playlista.append(dir_list[files])
+    if dir_list[files].lower().endswith(".mp3"):
+        playlista.append(dir_list[files])
+    else:
+        print(f"nie obsługiwany plik {dir_list[files]}")
     files += 1
+
+liczba = 1
+liczba2 = 0
 
 
 def kolejna_piosenka():
-    liczba = 1
+    global liczba, liczba2
+    pygame.mixer.music.stop()
+    pygame.mixer.music.unload()
     pygame.mixer.music.load(playlista[liczba])
+    zagraj()
     liczba += 1
+    liczba2 += 1
+
+
+checkbox_var = tk.IntVar()
 
 
 def check_event():
     for event in pygame.event.get():
         if event.type == MUSIC_END:
             running = True
-            while running:
-                if len(playlista) > 0:
-                    # liczba += 1
+            checkbox_var_final = checkbox_var.get()
+            print(checkbox_var_final)
+            if checkbox_var_final == 1:
+                zagraj()
+            elif checkbox_var_final == 0:
+                while running:
+                    if len(playlista) > 0:
+                        # buton.configure(text="►", command=zagraj)
+                        kolejna_piosenka()
+                        nazwa_pliku_var.set(playlista[liczba2])
+                        running = False
             print("aaa")
 
     app.after(1, check_event)
@@ -61,6 +85,7 @@ def unpauza():
 MUSIC_END = pygame.USEREVENT
 pygame.mixer.music.set_endevent(MUSIC_END)
 pygame.mixer.music.load(playlista[0])
+nazwa_pliku_var.set(playlista[liczba2])
 
 
 def zagraj():
@@ -76,13 +101,21 @@ trv = ttk.Treeview(
 trv.grid(row=0, column=1, padx=10, pady=10)
 
 trv.heading("utwor", text="Tytuł")
+liczba_plikow = 1
+for plik in playlista:
+    trv.insert("", tk.END, values=str(liczba_plikow) + "." + plik)
+    liczba_plikow += 1
 
+nazwa_pliku = tk.CTkLabel(app, textvariable=nazwa_pliku_var)
+nazwa_pliku.grid(row=1, column=1)
 buton = tk.CTkButton(app, text="►", command=zagraj, width=20, height=30)
 buton.grid(row=0, column=0)
+petla = tk.CTkCheckBox(app, text="Loop", variable=checkbox_var, onvalue=1, offvalue=0)
+petla.grid(row=0, column=1)
 
 check_event()
 
-a = 1
+a = 0.5
 
 
 def slider_event(value):
